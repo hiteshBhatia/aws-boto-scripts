cc53f87f5581ffdac0f1ee1ba9496be27de18cb3
Hitesh <hitesh@intelligrape.com>
2015-02-22 03:55:31 +0530

    minor changes in unfinished scripts rds and sg

---
+++ findRDSSnapshotsOlderThanXdays.py
@@ -0,0 +1,43 @@
+import boto.rds
+from datetime import date, timedelta,datetime
+from dateutil.parser import parse
+import csv
+
+days_counter=45
+region="ap-southeast-1"
+
+conn = boto.rds.connect_to_region(region)
+# amis=ec2conn.get_all_images(owners='self')
+
+print conn.get_all_dbsnapshots()
+print type(conn.get_all_dbsnapshots())
+
+# class Ami:
+#     def  __init__(self,ec2_ami):
+#         self.id = ec2_ami.id
+#         self.strpdate = parse(ec2_ami.creationDate).date()
+#
+#
+#
+# Computing Older date
+# old  = datetime.today() - timedelta(days_counter)
+# old_date=old.date()
+#
+# Fetching All AMI's
+# ec2conn = boto.ec2.connect_to_region(region)
+# amis=ec2conn.get_all_images(owners='self')
+#
+# print "Today's date : " + str(datetime.today())
+# print "Finding AMI's older than : " + str(old_date)
+#
+# older_list = []
+# for ami in amis:
+#     older_list = [ami for ami in amis if parse(ami.creationDate).date() < old_date ]
+#
+#
+#
+# for item in older_list:
+#     print item.id + "," + item.name + "," + item.creationDate
+#
+# print "Total : " + str(len(older_list))
+#
--- findUnusedSecurityGroup.py
+++ findUnusedSecurityGroup.py
@@ -1,1 +1,2 @@
+# Will need to check all the services in this case
 __author__ = 'hitesh'
